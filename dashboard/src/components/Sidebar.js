139774b75e2a933ade22969d196b7b882ad6e97d
Rajitha475 <104242998+Rajitha475@users.noreply.github.com>
2025-01-25 14:57:58 +0530

    Update Sidebar.js

--- dashboard/src/components/Sidebar.js
+++ dashboard/src/components/Sidebar.js
@@ -1,27 +1,59 @@
 import React from 'react';
-import './style.css';
+import { Link } from 'react-router-dom';
+import './style.css'; // Ensure you have the correct path to your CSS file
 
 function Sidebar() {
   return (
-    <div className="wrapper">
-      <div className="sidebar">
-        <h1>Bluestock Fintech</h1>
-        <ul>
-          <h2>MENU</h2>
-          <li><a href="#" className="active"><i className="fa-solid fa-chart-simple"></i>Dashboard</a></li>
-          <li><a href="#"><i className="fa-solid fa-cart-shopping"></i>Manage IPO</a></li>
-          <li><a href="#"><i className="fa-regular fa-chart-bar"></i>IPO Subscription</a></li>
-          <li><a href="#"><i className="fa-regular fa-comment"></i>IPO Allotment</a></li>
-          <h2>OTHERS</h2>
-          <li><a href="#"><i className="fa-solid fa-gear"></i>Settings</a></li>
-          <li><a href="#"><i className="fa-regular fa-floppy-disk"></i>API Manager</a></li>
-          <li><a href="#"><i className="fa-regular fa-user"></i>Accounts</a></li>
-          <li><a href="#"><i className="fa-regular fa-circle-question"></i>Help</a></li>
-        </ul>
-      </div>
-   </div>
-      
+    <div className="sidebar">
+      <h1>Bluestock Fintech</h1>
+      <h2>Menu</h2>
+      <ul>
+        <li>
+          <Link to="/" className="active">
+            <i className="fa-solid fa-chart-simple"></i> Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link to="/ManageIpo">
+            <i className="fa-solid fa-cart-shopping"></i> Manage IPO
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <i className="fa-regular fa-chart-bar"></i> IPO Subscription
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <i className="fa-regular fa-comment"></i> IPO Allotment
+          </Link>
+        </li>
+        <h2>Others</h2>
+        <li>
+          <Link to="#">
+            <i className="fa-solid fa-gear"></i> Settings
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <i className="fa-regular fa-floppy-disk"></i> API Manager
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <i className="fa-regular fa-user"></i> Accounts
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <i className="fa-regular fa-circle-question"></i> Help
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
 export default Sidebar;
+
+
