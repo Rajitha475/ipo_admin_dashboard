e0388ebdae00d1464db4c1f43bb5a456a6b88965
Rajitha475 <104242998+Rajitha475@users.noreply.github.com>
2025-02-14 18:23:00 +0530

    App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar'; // Import Sidebar
 import Dashboard from './components/Dashboard';
 import ManageIpo from './components/ManageIpo';
 import Header from "./components/Header";
 import RegisterIpo from './components/RegisterIpo'; 
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+import ForgotPassword from "./components/ForgotPass";
 import './components/style.css'; // Ensure correct path to CSS file
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const authStatus = localStorage.getItem("isAuthenticated") === "true";
+    setIsAuthenticated(authStatus);
+  }, []);
   return (
     <Router>
       <div className="app-container">
-      <Header />
-        <Sidebar /> {/* Use Sidebar here */}
+      {isAuthenticated && <Header />}
+        {isAuthenticated && <Sidebar />} {/* Use Sidebar here */}
         <div className="main-content">
           <Routes>
+          {isAuthenticated ? (
+              <>
             <Route path="/" element={<Dashboard />} />
             <Route path="/ManageIpo" element={<ManageIpo />} />
-            <Route path="/register-ipo" component={RegisterIpo} />
+            <Route path="/RegisterIpo" element={<RegisterIpo />} />
+            </>
+            ) : (
+              <>
+                <Route path="/LogIn" element={<LogIn onAuth={setIsAuthenticated} />} />
+                <Route path="/SignUp" element={<SignUp onAuth={setIsAuthenticated} />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="*" element={<Navigate to="/LogIn" />} />
+              </> 
+            )}
           </Routes>
+
         </div>
       </div>
     </Router>
